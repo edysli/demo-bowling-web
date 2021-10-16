0934e5ea32a21bd1fa4b9633ab0d1dafaf0c1b83
Etienne Dysli Metref <etienne.dysli@gmail.com>
2021-10-16 10:43:52 +0200

    Add fourth test of the bowling game kata

--- src/test/java/com/example/bowlingscore/BowlingScoreTest.java
+++ src/test/java/com/example/bowlingscore/BowlingScoreTest.java
@@ -3,7 +3,6 @@ package com.example.bowlingscore;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class BowlingScoreTest {
@@ -30,6 +29,19 @@ class BowlingScoreTest {
     assertThat(g.score(), is(16));
   }
 
+  @Test
+  void oneStrike() {
+    rollStrike();
+    g.roll(3);
+    g.roll(4);
+    rollMany(16, 0);
+    assertThat(g.score(), is(24));
+  }
+
+  private void rollStrike() {
+    g.roll(10);
+  }
+
   private void rollSpare() {
     g.roll(5);
     g.roll(5);
@@ -54,18 +66,38 @@ class Game {
     int score = 0;
     int frameIndex = 0;
     for (int frame = 0; frame < 10; frame++) {
-      if (isSpare(frameIndex)) {
-        score += 10 + rolls[frameIndex + 2];
+      if (isStrike(frameIndex)) {
+        score += 10 + strikeBonus(frameIndex);
+        frameIndex++;
+      } else if (isSpare(frameIndex)) {
+        score += 10 + spareBonus(frameIndex);
         frameIndex += 2;
       } else {
-        score += rolls[frameIndex] + rolls[frameIndex + 1];
+        score += sumOfBallsInFrame(frameIndex);
         frameIndex += 2;
       }
     }
     return score;
   }
 
+  private int sumOfBallsInFrame(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex + 1];
+  }
+
+  private int strikeBonus(int frameIndex) {
+    return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+  }
+
+  private int spareBonus(int frameIndex) {
+    return rolls[frameIndex + 2];
+  }
+
+  private boolean isStrike(int frameIndex) {
+    return rolls[frameIndex] == 10;
+  }
+
   private boolean isSpare(int frameIndex) {
     return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
   }
+
 }
